302fdf63137676d61c7f4689b3a9c53abde5dfe3
Bran Liang <lby89757@hotmail.com>
2016-07-28 14:07:18 +0800

    add first test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,11 @@
 # Your code here
+describe Calculator do
+
+  let(:c){ Calculator.new }
+
+  describe '#add' do
+    it 'should sum two avlues together' do
+      expect(c.add(1,2)).to eq(3)
+    end
+  end
+end
